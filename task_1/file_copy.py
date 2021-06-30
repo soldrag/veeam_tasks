acb653b8fee487d03d51288c8ebdf7c95cf7fba7
soldrag <soldrag@gmail.com>
2021-06-30 16:57:51 +0300

    add docstrings for task_1

--- task_1/file_copy.py
+++ task_1/file_copy.py
@@ -9,17 +9,28 @@ logger = logging.getLogger()
 
 
 def parse_xml(xml_file: str) -> List[Tuple[str, ...]]:
+    """
+    The function parses xml config file.
+    :param xml_file: path to xml file
+    :return: list of tuples with params
+    """
     tree = ET.parse(xml_file)
     root = tree.getroot()
     return [(item.get('source_path'), item.get('destination_path'), item.get('file_name')) for item in root]
 
 
 def check_exists_file(source_path: str, file_name: str) -> None:
+    """
+    Checking if file exists.
+    """
     if not isfile(join(source_path, file_name)):
         raise FileNotFoundError(f'File "{file_name}" not found.')
 
 
 def checking_for_create_folder(destination_path: str, create_folder_flag: bool) -> None:
+    """
+    Checking if path exists.
+    """
     if not isdir(destination_path):
         if create_folder_flag:
             os.mkdir(destination_path)
@@ -28,6 +39,9 @@ def checking_for_create_folder(destination_path: str, create_folder_flag: bool) 
 
 
 def checking_for_replace(destination_path: str, file_name: str, file_replace_flag: bool) -> bool:
+    """
+    Checking if need replace existed file. Displays a dialog to confirm if the flag for overwrite is not set.
+    """
     if file_replace_flag or not isfile(join(destination_path, file_name)):
         flag = True
     else:
@@ -37,13 +51,12 @@ def checking_for_replace(destination_path: str, file_name: str, file_replace_fla
 
 
 def copy_file(source_path: str, destination_path: str, file_name: str) -> None:
+    """Copy file to destination folder"""
     shutil.copy(join(source_path, file_name), destination_path)
 
 
-def main(xml_file: str,
-         error_ignore: bool = True,
-         folder_create: bool = False,
-         file_replace: bool = False):
+def main(xml_file: str, error_ignore: bool = True, folder_create: bool = False, file_replace: bool = False):
+    """The main function call all other functions and manages the process."""
     copy_items = parse_xml(xml_file)
     for item in copy_items:
         source_path, destination_path, file_name = item
@@ -60,7 +73,3 @@ def main(xml_file: str,
                 continue
             else:
                 raise err
-
-
-if __name__ == '__main__':
-    pass
